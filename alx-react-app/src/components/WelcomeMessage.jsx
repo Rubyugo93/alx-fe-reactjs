bfb61e9ab842538c54430ccf52d0a70804354e79
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-05 14:38:02 +0100

    Add files via upload

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+// WelcomeMessage.jsx
+import React from 'react';
+
+const WelcomeMessage = () => {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+};
+
+export default WelcomeMessage;
