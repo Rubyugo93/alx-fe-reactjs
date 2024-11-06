7ddc2d8b54fd3a5891934114abb42724ce49cbd0
Rosemary Ipuole <ipuolerosemary@gmail.com>
2024-11-06 09:40:02 +0100

    Initial commit for alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+// MainContent.jsx
+const MainContent = () => {
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', padding: '20px' }}>
+      <p style={{ fontSize: '1.2em', color: '#333' }}>
+        Welcome to my favorite cities app! Explore the cities I love.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
