b675dad693899ed05bd5d1f20dd6a5da709ccf8e
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-06 10:28:07 +0100

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+// src/components/Footer.jsx
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p>&copy; 2024 My Favorite Cities. All rights reserved.</p>
+    <footer className="footer">
+      <p>&copy; 2024 My Website. All Rights Reserved.</p>
     </footer>
   );
 };
