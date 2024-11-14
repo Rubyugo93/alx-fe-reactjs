5920ae64eab61686622fd7066969abbb377c8126
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-14 13:41:58 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,6 +4,7 @@ const MainContent = () => {
   return (
     <main style={{ padding: '2em', backgroundColor: '#eaeaea', borderRadius: '8px', textAlign: 'center' }}>
       <p>Welcome to my favorite cities collection. Here, you'll find amazing places around the world!</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 };
