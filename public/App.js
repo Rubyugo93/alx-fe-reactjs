7bb981caf4d56772834209ab0fd3d699ec859ab9
Rosemary Ipuole <ipuolerosemary@gmail.com>
2024-11-19 20:08:00 +0100

    Integrate React Query for data fetching, caching, and updating

---
+++ public/App.js
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './PostsComponent';  // The component you'll create
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
