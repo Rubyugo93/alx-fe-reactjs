60c01c0bbb8ceceafa05afddd4b9a27c389e96cf
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-21 09:47:32 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,18 @@
 // src/App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import SearchBar from './components/SearchBar';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetail from './components/RecipeDetail';
 
 const App = () => {
   return (
-    <Router>
-      <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
-        <h1 style={{ textAlign: 'center', color: '#333' }}>Recipe Sharing Application</h1>
-        <AddRecipeForm />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Recipe Sharing Application</h1>
+      <SearchBar /> {/* Added SearchBar here */}
+      <AddRecipeForm />
+      <RecipeList /> {/* RecipeList will show filtered results */}
+    </div>
   );
 };
 
