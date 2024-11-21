7b6a3980dc51510036ca8e31877340801e74facf
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-21 09:25:51 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,27 +1,25 @@
+// src/recipeStore.js
 import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-
+  
   // Add a new recipe
-  addRecipe: (recipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, recipe],
-    })),
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe],
+  })),
 
   // Delete a recipe
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
 
   // Update an existing recipe
-  updateRecipe: (recipeId, updatedData) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === recipeId ? { ...recipe, ...updatedData } : recipe
-      ),
-    })),
+  updateRecipe: (recipeId, updatedData) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === recipeId ? { ...recipe, ...updatedData } : recipe
+    ),
+  })),
 }));
 
 export { useRecipeStore };
