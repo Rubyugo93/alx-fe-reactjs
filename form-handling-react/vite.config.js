74eeaf3568bc49a711e238bf8b44285acd0a0cbe
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-11-24 21:31:25 +0100

    Update vite.config.js

--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx', // Treat `.js` files as JSX
-    include: /src\/.*\.js$/, // Process only `.js` files in the `src` directory
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Ensure Vite resolves `.js` and `.jsx`
-  },
-});
+})
