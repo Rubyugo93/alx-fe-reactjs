d46371886e971a4b9056ab881ef067917d4df150
Rubyugo93 <166714967+Rubyugo93@users.noreply.github.com>
2024-12-12 18:33:05 +0100

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function UserProfile({ name, age, bio }) {
+function UserProfile ["{props.name}", "{props.age}", "{props.bio}"] {
   return (
     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', backgroundColor: '#f4f4f4', borderRadius: '8px' }}>
       <h2 style={{ color: 'blue' }}>{name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold' }}>{age}</span></p>
       <p>Bio: {bio}</p>
+       
     </div>
   );
 }
